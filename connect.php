f395355630da1fb48ed92939266d4616730ea0b9
YagnikPithva <yagnikpithva31@gmail.com>
2025-03-22 16:14:50 +0530

    update CORS implementation to allow all origins and update script and stylesheet sources in index.html

--- connect.php
+++ connect.php
@@ -1,35 +1,10 @@
 
 <?php
 
-function cors() {
-    $allowed_origins = [
-       "https://khodiyarlab.com",
-        "https://www.khodiyarlab.com",
-        "http://khodiyarlab.com",
-        "http://www.khodiyarlab.com",
-        "http://localhost:8080",
-        "http://localhost:4173",
-    ];
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
-    if (isset($_SERVER['HTTP_ORIGIN'])) {
-        $origin = rtrim($_SERVER['HTTP_ORIGIN'], '/'); // Ensure uniform comparison
-
-        if (in_array($origin, $allowed_origins)) {
-            header("Access-Control-Allow-Origin: $origin");
-            header("Access-Control-Allow-Credentials: true");
-            header("Access-Control-Max-Age: 86400"); // Cache for 1 day
-        }
-    }
-
-    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-        header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
-        http_response_code(204); // No Content
-        exit();
-    }
-}
-
-cors();
 
 $servername = "localhost";
 $username   = "zoqiebvw_admin";
