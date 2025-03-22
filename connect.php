bd59c1527ed06187789b73e29028b153fe2db981
YagnikPithva <yagnikpithva31@gmail.com>
2025-03-22 14:58:00 +0530

    add CORS support with specific origins and update script source in index.html

--- connect.php
+++ connect.php
@@ -2,32 +2,34 @@
 <?php
 
 function cors() {
-    
-    // Allow from any origin
+    $allowed_origins = [
+       "https://khodiyarlab.com",
+        "https://www.khodiyarlab.com",
+        "http://khodiyarlab.com",
+        "http://www.khodiyarlab.com",
+        "http://localhost:8080",
+        "http://localhost:4173",
+    ];
+
     if (isset($_SERVER['HTTP_ORIGIN'])) {
-        // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
-        // you want to allow, and if so:
-        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-        header('Access-Control-Allow-Credentials: true');
-        header('Access-Control-Max-Age: 86400');    // cache for 1 day
+        $origin = rtrim($_SERVER['HTTP_ORIGIN'], '/'); // Ensure uniform comparison
+
+        if (in_array($origin, $allowed_origins)) {
+            header("Access-Control-Allow-Origin: $origin");
+            header("Access-Control-Allow-Credentials: true");
+            header("Access-Control-Max-Age: 86400"); // Cache for 1 day
+        }
     }
-    
-    // Access-Control headers are received during OPTIONS requests
+
     if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-        
-        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-            // may also be using PUT, PATCH, HEAD etc
-            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-        
-        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-            header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-    
-        exit(0);
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+        header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+        http_response_code(204); // No Content
+        exit();
     }
 }
 
 cors();
-
 
 $servername = "localhost";
 $username   = "zoqiebvw_admin";
