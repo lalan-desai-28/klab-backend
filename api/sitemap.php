64a17560438ad312b50f0e048b24d79a4fe53adc
lalan-desai-28 <desailalan02@gmail.com>
2025-04-05 09:16:14 +0530

    bug fix

--- api/sitemap.php
+++ api/sitemap.php
@@ -1,30 +1,50 @@
 <?php
+// Turn off error reporting on output to avoid corrupting XML
+error_reporting(0);
+ini_set('display_errors', 0);
+
+// Set XML content-type header
 header("Content-type: text/xml");
+
+// Include your DB connection
 include "../../connect.php";
 
-echo '<?xml version="1.0" encoding="UTF-8"?>';
-echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+// Start building XML string
+$xml = '<?xml version="1.0" encoding="UTF-8"?>';
+$xml .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
 
-// Main Pages
+// Static pages
 $pages = [
     'https://khodiyarlab.com/',
     'https://khodiyarlab.com/products',
 ];
 
 foreach ($pages as $page) {
-    echo "<url><loc>$page</loc><priority>1.0</priority><changefreq>daily</changefreq></url>";
+    $xml .= '<url>';
+    $xml .= '<loc>' . htmlspecialchars($page) . '</loc>';
+    $xml .= '<priority>1.0</priority>';
+    $xml .= '<changefreq>daily</changefreq>';
+    $xml .= '</url>';
 }
 
-// Fetch product URLs from database
+// Fetch product links from DB
 $sql = "SELECT id FROM products ORDER BY id ASC";
 $result = $conn->query($sql);
 
 if ($result && $result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
-        $productId = $row['id'];
-        echo "<url><loc>https://khodiyarlab.com/product/$productId</loc><priority>0.8</priority><changefreq>weekly</changefreq></url>";
+        $productUrl = 'https://khodiyarlab.com/product/' . $row['id'];
+        $xml .= '<url>';
+        $xml .= '<loc>' . htmlspecialchars($productUrl) . '</loc>';
+        $xml .= '<priority>0.8</priority>';
+        $xml .= '<changefreq>weekly</changefreq>';
+        $xml .= '</url>';
     }
 }
 
-echo '</urlset>';
+// Close the root tag
+$xml .= '</urlset>';
+
+// Output the XML
+echo $xml;
 ?>
